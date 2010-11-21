9161e8f27411d060d17825022a79cadb63e2c130
Laurent <foxp2projects@googlemail.com>
2010-11-21 20:49:49 +0100

    update : (A)+(UP)+Add+Support+for+Basic+HTTP+Authentication

--- catalog/admin/includes/languages/french/administrators.php
+++ catalog/admin/includes/languages/french/administrators.php
@@ -13,6 +13,7 @@
 define('HEADING_TITLE', 'Administrateurs');
 
 define('TABLE_HEADING_ADMINISTRATORS', 'Administrateurs');
+define('TABLE_HEADING_HTPASSWD', 'Protégé par htpasswd');
 define('TABLE_HEADING_ACTION', 'Action');
 
 define('TEXT_INFO_INSERT_INTRO', 'Entrez les informations du nouvel administrateur');
@@ -24,4 +25,8 @@ define('TEXT_INFO_NEW_PASSWORD', 'Nouveau Mot de Passe :');
 define('TEXT_INFO_PASSWORD', 'Mot de Passe :');
 
 define('ERROR_ADMINISTRATOR_EXISTS', 'Erreur : Administrateur déjà existant.');
+define('HTPASSWD_INFO', '<strong>Sécurisation via htaccess/htpasswd</strong><p>Cette installation de l\'outil d\'administration de boutique osCommerce n\'utilise pas de sécurisation via htaccess/htpasswd.</p><p>Activer la sécurisation via htaccess/htpasswd stockera automatiquement le nom d\'utilisateur et le mot de passe de l\'administrateur dans un fichier htpasswd lors de la mise à jour du mot de passe de l\'administrateur.</p><p><strong>Veuillez noter</strong>, si cette protection est activée et que vous ne pouvez plus accéder à l\'outil d\'administration, veuillez apporter les modifications suivantes et consultez votre hébergeur pour permettre l\'utilisation de la sécurisation via htaccess/htpasswd:</p><p><u><strong>1. Modifier ce fichier:</strong></u><br /><br />' . DIR_FS_ADMIN . '.htaccess</p><p>Supprimer les lignes suivantes si elles existent:</p><p><i>%s</i></p><p><u><strong>2. Supprimer ce fichier:</strong></u><br /><br />' . DIR_FS_ADMIN . '.htpasswd_oscommerce</p>');
+define('HTPASSWD_SECURED', '<strong>Sécurisation via htaccess/htpasswd</strong><p>Cette installation de l\'outil d\'administration de boutique osCommerce utilise la sécurisation via htaccess/htpasswd.</p>');
+define('HTPASSWD_PERMISSIONS', '<strong>Sécurisation via htaccess/htpasswd</strong><p>Cette installation de l\'outil d\'administration de boutique osCommerce n\'utilise pas de sécurisation via htaccess/htpasswd.</p><p>Les fichiers doivent être accessibles en écriture pour le serveur web pour activer la sécurisation via htaccess/htpasswd:</p><ul><li>' . DIR_FS_ADMIN . '.htaccess</li><li>' . DIR_FS_ADMIN . '.htpasswd_oscommerce</li></ul><p>Rafraichissez la page pour verifier si les droits sur les fichiers ont été correctement paramétrés.</p>');
+
 ?>
